32d244d0a07a733a46829237c18ba27bc08cd7cb
kadiirkkoc <you@example.com>
2024-04-27 02:10:46 +0300

    first commit

---
+++ src/main/java/system/colluagemanagement/ColluageManagementApplication.java
@@ -0,0 +1,13 @@
+package system.colluagemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColluageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColluageManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/system/colluagemanagement/ColluageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package system.colluagemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColluageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
